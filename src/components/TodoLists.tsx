7d489615b5a8c5c583689a424d18684b012f3465
Qihang Tian <17811951+immortalt@users.noreply.github.com>
2023-03-16 14:13:13 -0700

    feat(all): achieve the normal click effect and long-press draggable list effect without conflict; add count text on item

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -5,36 +5,44 @@ import {IonItem, IonLabel} from "@ionic/react";
 
 interface Item {
     id: string;
-    content: string;
+    title: string;
+    count: number;
+    order: number;
 }
 
 const getItems = (count: number): Item[] =>
     Array.from({length: count}, (v, k) => k).map(k => ({
         id: `list-${k}`,
-        content: `List ${k}`
+        title: `List ${k}`,
+        count: k,
+        order: k
     }));
 
 const reorder = (list: Item[], startIndex: number, endIndex: number): Item[] => {
-    const result = Array.from(list);
+    const result = Array.from(list.sort((a, b) => a.order - b.order));
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
-
+    // switch the order of the items
+    const temp = result[startIndex].order;
+    result[startIndex].order = result[endIndex].order;
+    result[endIndex].order = temp;
+    // TODO: send a request to update the order of the items in the database
     return result;
 };
 
-
 const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
     userSelect: "none",
+    cursor: "default",
     ...draggableStyle
 });
 
 const getListStyle = (isDraggingOver: boolean) => ({
-    width: "100%"
+    width: "100%",
+    cursor: "default",
 });
 
 const TodoLists: React.FC = () => {
     const [items, setItems] = useState<Item[]>(getItems(20));
-
     const onDragEnd = (result: any) => {
         if (!result.destination) {
             return;
@@ -61,16 +69,17 @@ const TodoLists: React.FC = () => {
                         {items.map((item, index) => (
                             <Draggable key={item.id} draggableId={item.id} index={index}>
                                 {(provided, snapshot) => (
-                                    <IonItem ref={provided.innerRef}
+                                    <IonItem button detail={false} ref={provided.innerRef}
                                              {...provided.draggableProps}
                                              {...provided.dragHandleProps} style={getItemStyle(
                                         snapshot.isDragging,
                                         provided.draggableProps.style
                                     )}
-                                             color={snapshot.isDragging ? "primary" : ""}
                                              lines="none"
+                                             color={snapshot.isDragging ? "light" : ""}
                                     >
-                                        <IonLabel>{item.content}</IonLabel>
+                                        <IonLabel>{item.title}</IonLabel>
+                                        <IonLabel slot="end">{item.count}</IonLabel>
                                     </IonItem>
                                 )}
                             </Draggable>
